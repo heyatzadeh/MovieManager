2e740b9bb4fccd7b367f3615372e1fa1a929b556
Hossein <hheyatzadeh@gmail.com>
2022-10-29 21:47:29 +0330

    adding first version

---
+++ MovieDataFetcherRunner/MovieDataFetcherRunner.cpp
@@ -0,0 +1,30 @@
+#include "stdafx.h"
+#include <iostream>
+
+#include "../MovieDataFetcher/MetaDataProcessor.h"
+#include "../MovieDataFetcher/RestApiClient.h"
+#include "../MovieDataFetcher/TheMovieDbDataFactory.h"
+#include "../MovieDataFetcher/TheMovieDbRepository.h"
+#include "../MovieDataFetcherTests/ApiKey.h"
+
+int main()
+{
+    TheMovieDbDataFactory dataFactory;
+    RestApiClient client;
+    TheMovieDbRepository repository(MY_API_KEY, dataFactory, client);
+    MetaDataProcessor processor(repository);
+
+    Movies result;
+    processor.ProcessMovies({"Batman Begins"}, result);
+
+    for (auto movie : result)
+    {
+        std::cout << "Title: " << movie->GetTitle() << std::endl;
+        std::cout << "IMDB Id: " << movie->GetImdbId() << std::endl << std::endl;
+        std::cout << movie->GetPlot() << std::endl << std::endl;
+        std::cout << "Length (min.) " << movie->GetLengthMin() << std::endl << std::endl;
+        std::cout << "----------------------------------------" << std::endl;
+    }
+
+    return 0;
+}
---
+++ MovieDataFetcherTests/ApiKey.h
@@ -0,0 +1,4 @@
+#pragma once
+#include <string>
+
+const std::string MY_API_KEY = "6fa8a41afcc0867b9b46683f978a4be5";
