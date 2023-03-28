b275fdbaaac2fcecacdc1483dac83e67fcc15325
Hossein <hheyatzadeh@gmail.com>
2023-03-28 15:32:38 +0330

    adding doxygen

--- src/MovieDataFetcher/RestApiClient.h
+++ src/MovieDataFetcher/RestApiClient.h
@@ -1,11 +1,17 @@
 #pragma once
+
 #include <string>
 
-class RestApiClient
-{
+class RestApiClient {
 public:
-	RestApiClient();
-	virtual ~RestApiClient();
+    RestApiClient();
 
-	virtual std::string HttpGet(std::string& url);
+    virtual ~RestApiClient();
+
+    /**
+     * @brief performs a get request to the url given
+     * @param url
+     * @return std::string html response
+     */
+    virtual std::string HttpGet(std::string &url);
 };
